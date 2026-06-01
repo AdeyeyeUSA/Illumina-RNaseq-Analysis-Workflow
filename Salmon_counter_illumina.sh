1a41971958f4a6d08860ef1966a230aa2006026c
Ade <156101575+AdeyeyeUSA@users.noreply.github.com>
2026-06-01 13:17:48 -0500

    Add files via upload

---
+++ Salmon_counter_illumina.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+# Run Salmon quantification
+
+salmon quant -i /home/ade/Renemma_2/mRatBN7_index \
+             -l A \
+             -1 /home/ade/Renemma_2/CleanData/LCS10337_PR1_Clean_Data1.fq.gz \
+             -2 /home/ade/Renemma_2/CleanData/LCS10337_PR1_Clean_Data2.fq.gz \
+             -p 28 \
+             --validateMappings \
+             -o /home/ade/Renemma_2/PR1
+
+salmon quant -i /home/ade/Renemma_2/mRatBN7_index \
+             -l A \
+             -1 /home/ade/Renemma_2/CleanData/LCS10337_PR2_Clean_Data1.fq.gz \
+             -2 /home/ade/Renemma_2/CleanData/LCS10337_PR2_Clean_Data2.fq.gz \
+             -p 28 \
+             --validateMappings \
+             -o /home/ade/Renemma_2/PR2
+
+salmon quant -i /home/ade/Renemma_2/mRatBN7_index \
+             -l A \
+             -1 /home/ade/Renemma_2/CleanData/LCS10337_PR3_Clean_Data1.fq.gz \
+             -2 /home/ade/Renemma_2/CleanData/LCS10337_PR3_Clean_Data2.fq.gz \
+             -p 28 \
+             --validateMappings \
+             -o /home/ade/Renemma_2/PR3
+
+salmon quant -i /home/ade/Renemma_2/mRatBN7_index \
+             -l A \
+             -1 /home/ade/Renemma_2/CleanData/LCS10337_PR4_Clean_Data1.fq.gz \
+             -2 /home/ade/Renemma_2/CleanData/LCS10337_PR4_Clean_Data2.fq.gz \
+             -p 28 \
+             --validateMappings \
+             -o /home/ade/Renemma_2/PR4
+
+salmon quant -i /home/ade/Renemma_2/mRatBN7_index \
+             -l A \
+             -1 /home/ade/Renemma_2/CleanData/LCS10337_PR5_Clean_Data1.fq.gz \
+             -2 /home/ade/Renemma_2/CleanData/LCS10337_PR5_Clean_Data2.fq.gz \
+             -p 28 \
+             --validateMappings \
+             -o /home/ade/Renemma_2/PR5
+
+salmon quant -i /home/ade/Renemma_2/mRatBN7_index \
+             -l A \
+             -1 /home/ade/Renemma_2/CleanData/LCS10337_PR6_Clean_Data1.fq.gz \
+             -2 /home/ade/Renemma_2/CleanData/LCS10337_PR6_Clean_Data2.fq.gz \
+             -p 28 \
+             --validateMappings \
+             -o /home/ade/Renemma_2/PR6
+
+salmon quant -i /home/ade/Renemma_2/mRatBN7_index \
+             -l A \
+             -1 /home/ade/Renemma_2/CleanData/LCS10337_PR7_Clean_Data1.fq.gz \
+             -2 /home/ade/Renemma_2/CleanData/LCS10337_PR7_Clean_Data2.fq.gz \
+             -p 28 \
+             --validateMappings \
+             -o /home/ade/Renemma_2/PR7
+
+salmon quant -i /home/ade/Renemma_2/mRatBN7_index \
+             -l A \
+             -1 /home/ade/Renemma_2/CleanData/LCS10337_PR8_Clean_Data1.fq.gz \
+             -2 /home/ade/Renemma_2/CleanData/LCS10337_PR8_Clean_Data2.fq.gz \
+             -p 28 \
+             --validateMappings \
+             -o /home/ade/Renemma_2/PR8
+
+salmon quant -i /home/ade/Renemma_2/mRatBN7_index \
+             -l A \
+             -1 /home/ade/Renemma_2/CleanData/LCS10337_PR9_Clean_Data1.fq.gz \
+             -2 /home/ade/Renemma_2/CleanData/LCS10337_PR9_Clean_Data2.fq.gz \
+             -p 28 \
+             --validateMappings \
+             -o /home/ade/Renemma_2/PR9
+
+salmon quant -i /home/ade/Renemma_2/mRatBN7_index \
+             -l A \
+             -1 /home/ade/Renemma_2/CleanData/LCS10337_PR10_Clean_Data1.fq.gz \
+             -2 /home/ade/Renemma_2/CleanData/LCS10337_PR10_Clean_Data2.fq.gz \
+             -p 28 \
+             --validateMappings \
+             -o /home/ade/Renemma_2/PR10
+
+salmon quant -i /home/ade/Renemma_2/mRatBN7_index \
+             -l A \
+             -1 /home/ade/Renemma_2/CleanData/LCS10337_PR11_Clean_Data1.fq.gz \
+             -2 /home/ade/Renemma_2/CleanData/LCS10337_PR11_Clean_Data2.fq.gz \
+             -p 28 \
+             --validateMappings \
+             -o /home/ade/Renemma_2/PR11
+
+salmon quant -i /home/ade/Renemma_2/mRatBN7_index \
+             -l A \
+             -1 /home/ade/Renemma_2/CleanData/LCS10337_PR12_Clean_Data1.fq.gz \
+             -2 /home/ade/Renemma_2/CleanData/LCS10337_PR12_Clean_Data2.fq.gz \
+             -p 28 \
+             --validateMappings \
+             -o /home/ade/Renemma_2/PR12
